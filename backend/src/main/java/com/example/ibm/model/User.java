733f4870533e1f6d87be5035634a3b55d6b4f6d2
PranavPrak <prakash.pran@northeastern.edu>
2024-04-02 01:05:45 -0400

    Updated the models with the new dependancies

--- backend/src/main/java/com/example/ibm/model/User.java
+++ backend/src/main/java/com/example/ibm/model/User.java
@@ -1,12 +1,12 @@
 package com.example.ibm.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 public class User {
